79ded076ff7a74cfc71e59ff4a2f0dd29373363b
hash243 <63360680+hash243@users.noreply.github.com>
2021-06-18 14:53:58 +0530

    Update install_winrar.bat

--- Files/install_winrar.bat
+++ Files/install_winrar.bat
@@ -1,5 +1,7 @@
 :: cmd /c ""C:\Program Files (x86)\Microsoft Visual Studio\Installer\resources\app\layout\InstallCleanup.exe" -f"
 cd C:\Users\Public\Desktop
+curl -o "C:\Users\Public\Desktop\winrar-x64-602.exe" https://raw.githubusercontent.com/hash243/nrd/main/Files/winrar-x64-602.exe > out.txt 2>&1
+curl -o "C:\Users\Public\Desktop\rarreg.key" https://raw.githubusercontent.com/hash243/nrd/main/Files/rarreg.key > out.txt 2>&1
 echo Installing WinRAR
 start "" /w "winrar-x64-602.exe" /s > out.txt 2>&1
 move "rarreg.key" "C:\Program Files\WinRAR" > out.txt 2>&1
