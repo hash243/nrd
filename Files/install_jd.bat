0c21ef3b40b8d71dc65f5691debe23990ce24f66
hash243 <63360680+hash243@users.noreply.github.com>
2021-07-10 17:08:47 +0530

    Update install_jd.bat

--- Files/install_jd.bat
+++ Files/install_jd.bat
@@ -2,3 +2,4 @@ cd C:\Users\Public\Desktop
 curl -o "C:\Users\Public\Desktop\Jd2.rar" https://raw.githubusercontent.com/hash243/nrd/main/Files/Jd2.rar
 start "" /w "%ProgramFiles%\WinRAR\winrar.exe" x -ibck C:\Users\Public\Desktop\Jd2.rar *.* C:\
 move "C:\Jdownloader2\JDownloader2.lnk" "C:\Users\Public\Desktop"
+del "C:\Users\Public\Desktop\Jd2.rar"
