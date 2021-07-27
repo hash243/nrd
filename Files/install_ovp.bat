37bcbf24e4b0921d559f3c288c677747200cc362
hash243 <63360680+hash243@users.noreply.github.com>
2021-07-27 17:46:32 +0530

    Create install_ovp.bat

---
+++ Files/install_ovp.bat
@@ -0,0 +1,5 @@
+cd C:\Users\Public\Desktop
+curl -o "C:\Users\Public\Desktop\OpenVPN-2.5.3-I601-amd64.msi" https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.3-I601-amd64.msi
+echo Installing OpenVPN
+start "" /w msiexec /i OpenVPN-2.5.3-I601-amd64.msi /S > out.txt  2>&1
+del "C:\Users\Public\Desktop\OpenVPN-2.5.3-I601-amd64.msi"
