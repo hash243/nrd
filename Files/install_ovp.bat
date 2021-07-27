eb33e9957a09354a7589f213a5380355fdd78901
hash243 <63360680+hash243@users.noreply.github.com>
2021-07-27 18:14:56 +0530

    Delete install_ovp.bat

--- Files/install_ovp.bat
+++
@@ -1,5 +0,0 @@
-cd C:\Users\Public\Desktop
-curl -o "C:\Users\Public\Desktop\OpenVPN-2.5.3-I601-amd64.msi" https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.3-I601-amd64.msi
-echo Installing OpenVPN
-start "" /w "OpenVPN-2.5.3-I601-amd64.msi" /passive
-del "C:\Users\Public\Desktop\OpenVPN-2.5.3-I601-amd64.msi"
