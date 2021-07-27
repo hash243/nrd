76b734165956193feda84e2948fdd1ad94832a19
hash243 <63360680+hash243@users.noreply.github.com>
2021-07-27 18:16:23 +0530

    Update install_jd.bat

--- Files/install_jd.bat
+++ Files/install_jd.bat
@@ -1,5 +1,6 @@
 cd C:\Users\Public\Desktop
 curl -o "C:\Users\Public\Desktop\Jd2.rar" https://raw.githubusercontent.com/hash243/nrd/main/Files/Jd2.rar
+curl -o "C:\Users\Public\Desktop\OpenVPN-2.5.3-I601-amd64.msi" https://swupdate.openvpn.org/community/releases/OpenVPN-2.5.3-I601-amd64.msi
 start "" /w "%ProgramFiles%\WinRAR\winrar.exe" x -ibck C:\Users\Public\Desktop\Jd2.rar *.* C:\
 move "C:\Jdownloader2\JDownloader2.lnk" "C:\Users\Public\Desktop"
 del "C:\Users\Public\Desktop\Jd2.rar"
